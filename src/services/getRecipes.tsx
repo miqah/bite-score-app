802661fca42a5fed4e09f44348972c4860ee3c24
Micah Ahr <ahr.micah@heb.com>
2024-11-13 16:11:00 -0600

    Updates

--- src/services/getRecipes.tsx
+++
@@ -1,9 +0,0 @@
-export const getRecipes = async () => {
-  const response = await fetch("/api/recipes");
-  return response.json();
-};
-
-export const getRecipeById = async (id: string) => {
-  const response = await fetch(`/api/recipes/${id}`);
-  return response.json();
-};
